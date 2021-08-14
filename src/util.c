f1b0a40dfd9bdd145179d62d5eb3702be8232195
waldemar <hellodoge@tuta.io>
2021-08-14 19:31:30 +0300

    (util) add "TODO graceful shutdown" notice

--- src/util.c
+++ src/util.c
@@ -24,6 +24,8 @@ void access_log(size_t thread_number, http_request_t *request, http_response_t *
 #undef TIME_BUFFER_LEN // access_log
 
 void signal_handler() {
+    // warning: exit is not thread safe and must not be used here
+    // TODO: graceful shutdown
     exit(0);
 }
 
